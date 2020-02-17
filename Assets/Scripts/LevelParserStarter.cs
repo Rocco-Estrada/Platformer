79914c72d7577fdc3c17fbe8ce0fec4cb351b3c4
Rocco-Estrada <roccoestrada@gmail.com>
2020-02-16 18:54:01 -0800

    Added Textures

---
+++ Assets/Scripts/LevelParserStarter.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelParserStarter : MonoBehaviour
+{
+    public string filename;
+
+    public GameObject Rock;
+
+    public GameObject Brick;
+
+    public GameObject QuestionBox;
+
+    public GameObject Stone;
+
+    public Transform parentTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshParse();
+    }
+
+
+    private void FileParser()
+    {
+        string fileToParse = string.Format("{0}{1}{2}.txt", Application.dataPath, "/Resources/", filename);
+
+        using (StreamReader sr = new StreamReader(fileToParse))
+        {
+            Vector3 pos = new Vector3();
+            string line = "";
+            int row = 0;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                int column = 0;
+                char[] letters = line.ToCharArray();
+                foreach (var letter in letters)
+                {
+                    pos.Set((float)column, (float)row * (-1.0f), 0);
+                    SpawnPrefab(letter, pos);
+                    column++;
+                }                
+                row++;
+            }
+
+            sr.Close();
+        }
+    }
+
+    private void SpawnPrefab(char spot, Vector3 positionToSpawn)
+    {
+        GameObject ToSpawn = new GameObject();
+
+        switch (spot)
+        {
+            case 'b': ToSpawn = Brick; break;
+            case '?': ToSpawn = QuestionBox; break;
+            case 'x': ToSpawn = Rock; break;
+            case 's': ToSpawn = Stone; break;
+            default: return;
+        }
+        ToSpawn = GameObject.Instantiate(ToSpawn, parentTransform);
+        ToSpawn.transform.rotation = Quaternion.Euler(0, 180, 0);
+        ToSpawn.transform.localPosition = positionToSpawn;
+    }
+
+    public void RefreshParse()
+    {
+        GameObject newParent = new GameObject();
+        newParent.name = "Environment";
+        newParent.transform.position = parentTransform.position;
+        newParent.transform.parent = this.transform;
+        
+        if (parentTransform) Destroy(parentTransform.gameObject);
+
+        parentTransform = newParent.transform;
+        FileParser();
+    }
+}
