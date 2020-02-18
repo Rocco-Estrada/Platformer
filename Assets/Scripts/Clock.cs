003da75190922a10d718ed66cacaabddb0a72088
Rocco-Estrada <roccoestrada@gmail.com>
2020-02-17 16:49:39 -0800

    UI and Countdown Implemented

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private Text clock;
+    private int time = 400;
+
+    void Start()
+    {
+        clock.text = time.ToString();
+    }
+
+    void Update()
+    {
+           
+        time--;
+        clock.text = time.ToString();
+    
+    }
+
+
+
+}
